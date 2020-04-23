c0eb64de53005887e454974c1c019178e339c4c5
diogosantosmendes <diogosantosmendes@hotmail.com>
2020-04-23 23:00:03 +0100

    work done

--- src/configs/mongodb.js
+++ src/configs/mongodb.js
@@ -1,16 +1,16 @@
-const MongoClient = require ('mongodb').MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 
-const mongodb = new MongoClient ('mongodb://localhost:27017');
+const mongodb = new MongoClient("mongodb://localhost:27017", { useUnifiedTopology: true });
 
 const connectDB = () => {
-  return new Promise ((resolve, reject) => {
-    mongodb.connect (err => {
-      if (err) reject (err);
-      else resolve ();
+  return new Promise((resolve, reject) => {
+    mongodb.connect((err) => {
+      if (err) reject(err);
+      else resolve();
     });
   });
 };
-const getDB = () => mongodb.db ('lessons');
-const disconnectDB = () => _db.close ();
+const getDB = () => mongodb.db("lessons");
+const disconnectDB = () => _db.close();
 
-module.exports = {connectDB, getDB, disconnectDB};
+module.exports = { connectDB, getDB, disconnectDB };
--- src/server.js
+++ src/server.js
@@ -1,29 +1,25 @@
-require ('./configs/mongodb.js')
-  .connectDB ()
-  .then (() => {
-    console.log (
-      `\x1b[32m(PLAIN) Successfuly connected to database server\x1b[0m`
-    );
+require("./configs/mongodb.js")
+  .connectDB()
+  .then(() => {
+    console.log(`\x1b[32m(PLAIN) Successfuly connected to database server\x1b[0m`);
 
-    const express = require ('express');
-    const bodyParser = require ('body-parser');
-    const cors = require ('cors');
+    const express = require("express");
+    const bodyParser = require("body-parser");
+    const cors = require("cors");
 
-    const bookRoute = require ('./routes/book-route.js');
+    const app = express();
 
-    const app = express ();
+    app.use(bodyParser.json());
+    app.use(cors());
 
-    app.use (bodyParser.json ());
-    app.use (cors ());
+    app.use("/book", require("./routes/book-route.js"));
+    app.use("/user", require("./routes/user-route.js"));
 
-    app.use ('/book', bookRoute);
-    app.use ('/home', (req, res) => res.json ({msg: 'HELLO'}));
-
-    const port = process.env.PORT || 3000;
-    app.listen (port, () => {
-      console.log (`\x1b[32m(PLAIN) Server listening on port ${port}\x1b[0m`);
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+      console.log(`\x1b[32m(PLAIN) Server listening on port ${port}\x1b[0m`);
     });
   })
-  .catch (err => {
-    console.error (err);
+  .catch((err) => {
+    console.error(err);
   });
